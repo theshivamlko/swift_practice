9ddfce0d2050a08bd3bd17f898285a0cb86d4da8
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-10-29 00:44:40 +0530

    init

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print(greeting)
+
+var age=31
+var weight=23.444
+weight=12
+
+print(age)
+print(weight)
+
+let salary=292.9  // constants
+(salary)
+ 
+
+var message:String="Shivam"
+
+let message2:String="Srivastava"
+print(message+" "+message2)
+print("\(message) \(message2)")
+
+message.append("_\(message2)");
+print("\(message)...")
+
+let printCar = "\(message)" + " Car"
+print("\(printCar)...")
+
+var result:Double = sqrt(625)
+print("SQRT \(result)")
+
+let first = 25.0
+let second = 3
+let result2 = Double(Int(first) * second)
+
+print("Convert \(result2)")
